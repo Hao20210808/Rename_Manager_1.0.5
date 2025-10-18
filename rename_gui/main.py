3150635a74cc925208424f49721142fdcb3b9578
Hao-Zhong <88621562+Hao20210808@users.noreply.github.com>
2025-10-19 02:37:33 +0800

    Delete rename_gui directory

--- rename_gui/main.py
+++
@@ -1,9 +0,0 @@
-from language_manager import load_language_setting, select_language
-from gui_main import main_app
-
-if __name__ == "__main__":
-    lang = load_language_setting()
-    if not lang:
-        select_language()
-        lang = load_language_setting()
-    main_app(lang)
